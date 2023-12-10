b36708b964a60468ef2fd48ff89c9300692fc31d
What <locdelorme@gmail.com>
2023-12-10 10:57:10 -0500

    update

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-var champ1 = document.getElementById('champ1');
-var cap1 = document.getElementById('cap1').innerHTML;
-var field1 = document.querySelector('.champ1')
+var champ1 = document.getElementById("champ1");
+var cap1 = document.getElementById("cap1").innerHTML;
+var field1 = document.querySelector(".champ1");
 
-var champ2 = document.getElementById('champ2');
-var cap2 = document.getElementById('cap2').innerHTML;
-var field2 = document.querySelector('.champ2')
+var champ2 = document.getElementById("champ2");
+var cap2 = document.getElementById("cap2").innerHTML;
+var field2 = document.querySelector(".champ2");
 
-var champ3 = document.getElementById('blurTrigger');
-var cap3 = document.getElementById('cap3').innerHTML;
-var field3 = document.querySelector('.champ3')
-var popup = document.getElementById('popup');
+var champ3 = document.getElementById("blurTrigger");
+var cap3 = document.getElementById("cap3").innerHTML;
+var field3 = document.querySelector(".champ3");
+var popup = document.getElementById("popup");
 
-var champ4 = document.getElementById('champ4');
-var cap4 = document.getElementById('cap4').innerHTML;
-var field4 = document.querySelector('.champ4')
-const popups = document.querySelectorAll('.popup');
-let dernierContenuChamp = ''; // Pour suivre le contenu précédent du champ
+var champ4 = document.getElementById("champ4");
+var cap4 = document.getElementById("cap4").innerHTML;
+var field4 = document.querySelector(".champ4");
+const popups = document.querySelectorAll(".popup");
+let dernierContenuChamp = ""; // Pour suivre le contenu précédent du champ
 
 // Interaction pour le champ 1 (afficher un popup choisi en priorité parmi ceux qui ne sont pas affichés)
 function afficherPopup() {
@@ -25,10 +25,13 @@ function afficherPopup() {
     if (contenuChamp.length > dernierContenuChamp.length) {
         // Filtrer les popups qui ne sont pas affichés
         var popupsNonAffiches = Array.from(popups).filter(function (popup) {
-            return popup.style.display === 'none';
+            return popup.style.display === "none";
         });
 
         // Si au moins un popup n'est pas affiché, choisir celui-ci en priorité
+
+        console.log(popupsNonAffiches);
+
         if (popupsNonAffiches.length > 0) {
             // Cacher tous les popups
             // popups.forEach(function (popup) {
@@ -36,65 +39,62 @@ function afficherPopup() {
             // });
 
             // Afficher le premier popup non affiché
-            popupsNonAffiches[0].style.display = 'block';
+            popupsNonAffiches[
+                Math.floor(Math.random() * popupsNonAffiches.length)
+            ].style.display = "block";
         } else {
             // Sinon, choisir un popup aléatoirement
             var indexPopupAleatoire = Math.floor(Math.random() * popups.length);
-            popups[indexPopupAleatoire].style.display = 'block';
+            popups[indexPopupAleatoire].style.display = "block";
         }
     }
 
     // Mettre à jour le suivi du contenu du champ
     dernierContenuChamp = contenuChamp;
 
-    console.log(cap1);
-    console.log(champ1.value);
-    if(cap1 == champ1.value){
+    if (cap1 == champ1.value) {
         field2.style.display = "block";
-        console.log(champ2)
-    };
-};
-
-
+    }
+}
 
 popups.forEach(function (popup) {
-    popup.style.display = 'none';
+    popup.style.display = "none";
     // Ajouter un gestionnaire d'événements pour fermer le popup lorsqu'il est cliqué
-    popup.addEventListener('click', function() {
-        popup.style.display = 'none';
+    popup.addEventListener("click", function () {
+        popup.style.display = "none";
     });
 });
 
 let champValue = "";
 const correspondanceClavier = {
-    'a': 'a',
-    'b': 'p',
-    'c': 'e',
-    'd': 'q',
-    'e': 'c',
-    'f': 't',
-    'g': 'j',
-    'h': 'n',
-    'i': 'i',
-    'j': 'g',
-    'k': 'k',
-    'l': 'l',
-    'm': 'm',
-    'n': 'h',
-    'o': 'o',
-    'p': 'b',
-    'q': 'd',
-    'r': 'r',
-    's': 's',
-    't': 'f',
-    'u': 'u',
-    'v': 'v',
-    'w': 'w',
-    'x': 'x',
-    'y': 'y',
-    'z': 'z'
+    a: "a",
+    b: "p",
+    c: "e",
+    d: "q",
+    e: "c",
+    f: "t",
+    g: "j",
+    h: "n",
+    i: "i",
+    j: "g",
+    k: "k",
+    l: "l",
+    m: "m",
+    n: "h",
+    o: "o",
+    p: "b",
+    q: "d",
+    r: "r",
+    s: "s",
+    t: "f",
+    u: "u",
+    v: "v",
+    w: "w",
+    x: "x",
+    y: "y",
+    z: "z",
 };
-var dernierContenu2 = '';
+var dernierContenu2 = "";
 var estEnTrainDeSupprimer2 = false;
 
 // Interaction pour le champ 3 (remapper le clavier)
@@ -110,7 +110,8 @@ function remapperClavier() {
     if (!estEnTrainDeSupprimer2) {
         // Remapper uniquement la dernière lettre
         var derniereLettre = texteEntree.charAt(texteEntree.length - 1);
-        var lettreRemappee = correspondanceClavier[derniereLettre] || derniereLettre;
+        var lettreRemappee =
+            correspondanceClavier[derniereLettre] || derniereLettre;
 
         // Mettre à jour le champ avec la lettre remappée
         champ2.value = texteEntree.slice(0, -1) + lettreRemappee;
@@ -120,24 +121,21 @@ function remapperClavier() {
     dernierContenu2 = champ2.value;
     estEnTrainDeSupprimer2 = false;
 
-    if(cap2 == champ2.value){
+    if (cap2 == champ2.value) {
         field3.style.display = "block";
-    };
+    }
 }
-document.getElementById('blurTrigger').addEventListener('focus', function() {
-    document.body.classList.add('blurred');
+document.getElementById("blurTrigger").addEventListener("focus", function () {
+    document.body.classList.add("blurred");
 });
 
-document.getElementById('blurTrigger').addEventListener('blur', function() {
-    document.body.classList.remove('blurred');
+document.getElementById("blurTrigger").addEventListener("blur", function () {
+    document.body.classList.remove("blurred");
 });
 
-console.log(champ3)
-
-document.getElementById('blurTrigger').addEventListener('input', function() {
-    if(cap3 == champ3.value){
+document.getElementById("blurTrigger").addEventListener("input", function () {
+    if (cap3 == champ3.value) {
         field4.style.display = "block";
-        document.body.classList.remove('blurred');
-    };
-    
+        document.body.classList.remove("blurred");
+    }
 });
